038980684afe5f3252d8574e05cb346cfaedbdfd
Gunar Schorcht <gunar@schorcht.net>
2020-03-24 12:25:45 +0100

    makefiles: add PCF957X driver pseudomodules

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -167,6 +167,12 @@ PSEUDOMODULES += mrf24j40m%
 PSEUDOMODULES += sx1272
 PSEUDOMODULES += sx1276
 
+# include variants of PCF857X drivers as pseudo modules
+PSEUDOMODULES += pcf857x_irq
+PSEUDOMODULES += pcf8574
+PSEUDOMODULES += pcf8574a
+PSEUDOMODULES += pcf8575
+
 # include variants of SHT1X drivers as pseudo modules
 PSEUDOMODULES += sht10
 PSEUDOMODULES += sht11
