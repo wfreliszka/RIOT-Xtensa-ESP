b768325a5c42be23fc6903b207fba169a7677353
Gunar Schorcht <gunar@schorcht.net>
2020-03-31 16:21:08 +0200

    boards/feather-m0: add ATWINC15x0 WiFi driver

--- boards/feather-m0/include/board.h
+++ boards/feather-m0/include/board.h
@@ -42,6 +42,20 @@ extern "C" {
 #define LED0_TOGGLE         (LED_PORT.OUTTGL.reg = LED0_MASK)
 /** @} */
 
+#ifdef MODULE_FEATHER_M0_WIFI
+/**
+ * @name    Board configuration for ATWINC15x0 WiFi netdev driver
+ * @{
+ */
+#define ATWINC15X0_PARAM_SPI            SPI_DEV(0)
+#define ATWINC15X0_PARAM_SSN_PIN        GPIO_PIN(0, 6)
+#define ATWINC15X0_PARAM_RESET_PIN      GPIO_PIN(0, 8)
+#define ATWINC15X0_PARAM_CHIP_EN_PIN    GPIO_PIN(0, 14)
+#define ATWINC15X0_PARAM_IRQ_PIN        GPIO_PIN(0, 21)
+#define ATWINC15X0_PARAM_WAKE_PIN       GPIO_UNDEF
+/** @} */
+#endif
+
 /**
  * @brief   Initialize board specific hardware, including clock, LEDs and std-IO
  */
